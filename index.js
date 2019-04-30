7d2acaed6304f49e2240f16bb1916386fc77b8c4
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2019-04-30 06:43:47 +0000

    bcrypt implementation

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const bearerToken = require('express-bearer-token');
 
 const app = express();
+
+app.use(bearerToken({
+    bodyKey: 'access_token',
+    queryKey: 'access_token',
+    headerKey: 'Bearer',
+    reqKey: 'token',
+    cookie: false,
+}));
 
 const sqlite3 = require('sqlite3');//importa la libreria
 var db = new sqlite3.Database('./DATABASE.sqlite3',(err) =>{
@@ -21,5 +29,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
 	res.send('Hello Express app');
 });
+
+
 
 app.listen(3000, () => console.log('server started'));
