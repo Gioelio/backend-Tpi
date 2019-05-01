5fe42d5fe39dbf684434b2823e46075be2237712
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2019-05-01 19:37:35 +0000

    metà

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const bodyParser = require('body-parser');
 const bearerToken = require('express-bearer-token');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const crypto = require('asymmetric-crypto');
+const keyPair = crypto.keyPair();
+const cors = require('cors');
+const jsonParser = bodyParser.json();
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bearerToken({
     bodyKey: 'access_token',
     queryKey: 'access_token',
@@ -32,19 +38,29 @@ app.get('/', (req, res) => {
 });
 
 app.route('/registration')
-    .post((req, res)=>{
+    .put(jsonParser,(req, res)=>{
         //req.nome, req.cognome, req.numero, req.password
         bcrypt.genSalt(10, (err, salt)=>{
             bcrypt.hash(req.password, salt,(err, hash)=>{
                 db.run('INSERT INTO Utente (?,?,?,?)', [req.nome, req.cognome, req.numero, hash], (err)=>{
                     if(err) res.staus(404).end()
-                    res.status(200).send(jwt.sign({})) //inserire dati nel token jwt
+                    res.status(200).send({'token': jwt.sign({'email': email}, keyPair.secretKey)}) //inserire dati nel token jwt
                 })
             })
         })
     })
-    .get((req,res) =>{
-
+    .post(jsonParser, (req,res) =>{
+        db.all('SELECT * FROM Utenti WHERE email = ?', [req.email], (err, result) =>{
+            if(err) throw err;
+            if(result.length === 0)
+                res.json({error: 'Non esiste nessun utente registrato con questa email'});
+            else
+                bcrypt.compare(req.password, result[0].password, (err, resCompare)=>{
+                    if(resCompare){
+                        res.json({'token': jwt.sign({'email': email}, keyPair.secretKey)})
+                    }
+                })
+        })
     })
 
 
